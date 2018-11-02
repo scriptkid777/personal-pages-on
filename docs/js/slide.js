32f9d1239a291795c3f41c44a6b1b92062b72304
Ivan <servsystem@yandex.ru>
2018-11-02 21:48:44 +0700

    2 commit

---
+++ docs/js/slide.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	var slideIndex = 0;
+	showPhotos();
+
+	function showPhotos(){
+		var i;
+		var slides = $('.photo img');
+
+		for ( i = 0; i < slides.length; i++) {
+			console.log(i);
+			$(slides[i]).hide();
+		}
+
+
+		slideIndex++;
+
+	
+
+		if (slideIndex > slides.length) {
+			slideIndex = 1;
+		}
+
+		$(slides[slideIndex-1]).show();
+
+		setTimeout(showPhotos, 800);
+
+		
+	}
+	
+});		
